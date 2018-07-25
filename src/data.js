92aac7090ffd42ba8238a3d996f2d45e8eed05cb
akhinchey <akhinchey@gmail.com>
2018-07-24 23:29:55 -0400

    Revert data ids.

--- src/data.js
+++ src/data.js
@@ -1,27 +1,27 @@
 const starterPosts = [
     {
-        id: 4,
+        id: 1,
         title: "Climb leg meow fish i must find my red catnip fishy fish",
         author: "Taco the Cat",
         text: "Leave hair on owner's clothes sleep everywhere, but not in my bed so the cat was chasing the mouse. Sleep in the bathroom sink make meme, make cute face and cat dog hate mouse eat string barf pillow no baths hate everything so scratch the postman wake up lick paw wake up owner meow meow or chase dog then run away eat owner's food. White cat sleeps on a black shirt always ensure to lay down in such a manner that tail can lightly brush human's nose yet meow to be let out pose purrfectly to show my beauty, so purr. ",
         replies: []
     },
     {
-        id: 3,
+        id: 2,
         title: "Chase after silly colored fish toys around the house purrr purr",
         author: "Kimbap who is also a cat",
         text: "Walk on car leaving trail of paw prints on hood and windshield fight an alligator and win, but slap owner's face at 5am until human fills food dish so lick human with sandpaper tongue for litter kitter kitty litty little kitten big roar roar feed me scratch at the door then walk away yet catch mouse and gave it as a present. Scratch me there, elevator butt sleep for claw at curtains stretch and yawn nibble on tuna ignore human bite human hand and sniff other cat's butt and hang jaw half open thereafter or you call this cat food, so hiding behind the couch until lured out by a feathery toy eat the fat cats food. ",
         replies: []
     },
     {
-        id: 2,
+        id: 3,
         title: "Plop down in the middle where everybody walks",
         author: "Maru the Famous Cat",
         text: "Hide from vacuum cleaner chew iPad power cord, pushes butt to face. The dog smells bad purr so cats secretly make all the worlds muffins sit and stare sleep on my human's head and stinky cat. Find something else more interesting meow go back to sleep owner brings food and water tries to pet on head, so scratch get sprayed by water because bad cat but eat the rubberband or my water bowl is clean and freshly replenished, so i'll drink from the toilet. Purr spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce see owner, run in terror catch mouse and gave it as a present need to chase tail, but mesmerizing birds or stand in front of the computer screen.",
         replies: []
     },
     {
-        id: 1,
+        id: 4,
         title: "Please stop looking at your phone and pet me leave hair on owner's clothes cough",
         author: "Grumpy Cat",
         text: "Chase laser lie on your belly and purr when you are asleep experiences short bursts of poo-phoria after going to the loo, yet hiss and stare at nothing then run suddenly away. Take a big fluffing crap 💩 this cat happen now, it was too purr-fect!!!, present belly, scratch hand when stroked. Making sure that fluff gets into the owner's eyes spread kitty litter all over house intently stare at the same spot pee in the shoe. Stretch sleep everywhere, but not in my bed and warm up laptop with butt lick butt fart rainbows until owner yells pee in litter box hiss at cats. Missing until dinner time nap all day, but kitty poochy.",
