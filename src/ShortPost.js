171cde7166867b8ec34ba6303765b83d00e67418
akhinchey <akhinchey@gmail.com>
2018-07-25 21:13:05 -0400

    Tweak ShortPost component.

--- src/ShortPost.js
+++ src/ShortPost.js
@@ -3,10 +3,11 @@ import React from 'react';
 const ShortPost = (props) => (
     <div>
         <h2>{props.title}</h2>
-        <h3>Author: {props.author}</h3>
         <p>
             {props.text}
         </p>
+        <h3>Author: {props.author}</h3>
+        <hr />
     </div>
 )
 
